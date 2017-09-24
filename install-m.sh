7629ee3ceffe51f71bfd99e26c7faf9a98bada2e
Wolf2000 <wolf2000@aon.at>
2017-09-24 12:37:11 +0200

    Update install-m.sh

--- install-m.sh
+++ install-m.sh
@@ -6,6 +6,6 @@ chmod +x temp info-bpi
 cp temp /usr/bin/
 cp info-bpi /usr/bin/
 #motd
-apt install dnsutils mawk toilet
+apt install dnsutils mawk toilet figlet
 echo  BpiBerry | toilet -f standard -F metal > /etc/motd
 cd
